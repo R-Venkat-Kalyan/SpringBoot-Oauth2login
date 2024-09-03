954af2c5d9bce800661edcb394a48023f87fbfd8
Reddy Venkat Kalyan <2100030959cseh@gmail.com>
2024-09-03 19:20:38 +0530

    updated oauth login

---
+++ SpringOAuth2/src/main/java/com/example/SpringOAuth2/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.SpringOAuth2;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+	
+//	@GetMapping({"/", "/index"})
+	@GetMapping("/")
+    public String index() {
+        return "index";  // Return the custom sign-in page template
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(Model model, @AuthenticationPrincipal OAuth2User principal) {
+        if (principal != null) {
+            model.addAttribute("name", principal.getAttribute("name"));
+            model.addAttribute("email", principal.getAttribute("email"));
+        }
+        return "welcome";
+    }
+}
